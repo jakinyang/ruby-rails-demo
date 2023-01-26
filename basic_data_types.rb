c1bafad4ec6c26138222ede6b894acd010df18c6
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-26 08:53:32 -0800

    --finish data types

--- basic_data_types.rb
+++ basic_data_types.rb
@@ -17,3 +17,28 @@ puts 6.even?
 puts 7.even?
 puts 6.odd?
 puts 7.odd?
+
+# String Methods
+puts "Hello" + " World"
+puts "Hello" << " World " << "Again"
+puts "Hello".concat(" Again ").concat("World")
+puts "Hello"[0]
+puts "Hello"[0..1]
+puts "Hello"[0,3]
+puts "Hello"[-2..-1]
+
+# Escape Characters
+puts "Hello\nWorld"
+
+puts "hello\sworld".capitalize
+puts "hello".include?("l")
+puts "hello".include?("z")
+
+puts "hello".upcase
+
+puts "hello".empty?
+puts "".empty?
+
+puts "hello\nworld".length
+
+puts "hello".reverse
