6c0d5d527a04a182509f3eb00e3ddd0e86127f92
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-26 08:29:45 -0800

    --add number examples

--- basic_data_types.rb
+++ basic_data_types.rb
@@ -1,1 +1,14 @@
 puts "Hello World!"
+
+# Numbers
+print 1 + 1
+print 2 - 2
+print 2 * 2
+print 10 / 5
+print 2 ** 2
+print 10 % 3
+
+# Number Methods
+print 13.to_f
+print 13.0.to_i
+print 13.9.to_i
